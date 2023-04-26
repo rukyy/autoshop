6cf5d201ce975927b1780c38c857e383a1b69973
Rukyy <rukemeniemogha38@gmail.com>
2023-04-26 13:00:13 +0100

    add a simple loader

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -11,6 +11,20 @@ const Home = () => {
 
     return ( 
         <div className="container">
+           {ispending && <div class="loader">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+                <div class="bar4"></div>
+                <div class="bar5"></div>
+                <div class="bar6"></div>
+                <div class="bar7"></div>
+                <div class="bar8"></div>
+                <div class="bar9"></div>
+                <div class="bar10"></div>
+                <div class="bar11"></div>
+                <div class="bar12"></div>
+            </div>}
             <div className="row">
                 {cars && <div className=" bootel">{cars&&cars.map((car)=>(
                     <div className=" col-12 col-md-4 col-lg-4 col-xl-4 card">
